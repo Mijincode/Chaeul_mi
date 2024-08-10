58047888e02aaa2d4ced8c525390de89390091e4
Mijin <mijindoig@hotmail.com>
2024-08-10 16:03:18 +1000

    Added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// src/App.js
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Gallery from "./components/Gallery";
@@ -7,25 +6,24 @@ import Booking from "./components/Booking";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import Review from "./components/Review";
-import FAQ from "./components/FAQ";
+import FAQs from "./components/FAQs";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/review" element={<Review />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/faq" element={<FAQ />} />
+        <Route path="/faqs" element={<FAQs />} />
       </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
