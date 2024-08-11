13b575492fa281767588ce050b1c5e77e11ec18e
Mijin <mijindoig@hotmail.com>
2024-08-11 23:56:42 +1000

    Add css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import logo from "../photos/logo2.jpg";
