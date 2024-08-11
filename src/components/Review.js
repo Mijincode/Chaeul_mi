22c382814cc7a44db1503d9a455399315415a41e
Mijin <mijindoig@hotmail.com>
2024-08-11 23:42:18 +1000

    Add className

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,41 +1,26 @@
 import React from "react";
 // import { useTranslation } from "react-i18next";
-import review1 from "../photos/reviews/review1.jpg";
-
-import review2 from "../photos/reviews/review2.jpg";
-import review3 from "../photos/reviews/review3.jpg";
-import review4 from "../photos/reviews/review4.jpg";
-import review5 from "../photos/reviews/review5.jpg";
-import review6 from "../photos/reviews/review6.jpg";
-import review7 from "../photos/reviews/review7.jpg";
-import review8 from "../photos/reviews/review8.jpg";
+import review1 from "../photos/reviews/image1.jpg";
+import review2 from "../photos/reviews/image2.jpg";
+import review3 from "../photos/reviews/image3.jpg";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { withTranslation } from "react-google-multi-lang";
+
 import "./styles.css";
 
 const Review = () => {
-  //   const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   // Array of review images
-  const reviewImages = [
-    review1,
-    review2,
-    review3,
-    review4,
-    review5,
-    review6,
-    review7,
-    review8,
-  ];
+  const reviewImages = [review1, review2, review3];
   return (
     <Row xs={2} md={4} className="g-4">
       {reviewImages.map((image, idx) => (
         <Col key={idx}>
           <Card>
             <Card.Body>
-              <Card.Img variant="top" src={image} />
+              <Card.Img variant="top" src={image} className="review-images" />
             </Card.Body>
           </Card>
         </Col>
@@ -43,4 +28,4 @@ const Review = () => {
     </Row>
   );
 };
-export default withTranslation(Review);
+export default Review;
