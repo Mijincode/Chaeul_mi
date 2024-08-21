5284c52a6c322d86b7c061f9e86c7c99a5998cec
Mijin <mijindoig@hotmail.com>
2024-08-21 22:39:51 +1000

    Add calendar

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import "./styles/Booking.css";
 
 const Booking = () => {
   const { t } = useTranslation();
-
+  const [value, onChange] = useState(new Date());
   return (
-    <div>
-      <h1>{t("booking.title")}</h1>
+    <div className="booking-container">
+      <h1 className="booking-title">{t("booking.title")}</h1>
       <p>{t("booking.selectDateTime")}</p>
       <p>{t("booking.availableHours")}</p>
+      <div>
+        <Calendar onChange={onChange} value={value} />
+      </div>
     </div>
   );
 };
