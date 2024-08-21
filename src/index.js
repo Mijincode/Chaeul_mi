df3fa91844071696407b8770b74b3c12644af826
Mijin <mijindoig@hotmail.com>
2024-08-21 22:40:59 +1000

    Delete debugging API key

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { TranslationProvider } from "react-google-multi-lang";
+
+import { APIProvider } from "@vis.gl/react-google-maps";
+
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
+
+console.log("Google Maps API Key:", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log("DEBUG - env", process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY);
 root.render(
   <React.StrictMode>
-    {/* <TranslationProvider
-      apiKey={process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY}
-      defaultLanguage="ko"
-    > */}
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-    {/* </TranslationProvider> */}
+    <APIProvider
+      apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </APIProvider>
   </React.StrictMode>
 );
 reportWebVitals();
