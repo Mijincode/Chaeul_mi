7bd8b598e9c22e808ddb5d5916bbd0c092d279e1
Mijin <mijindoig@hotmail.com>
2024-08-22 23:01:13 +1000

    Add GooglePam

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import map from "../photos/map.png";
+// import map from "../photos/map.png";
 import kakaotalk from "../photos/kakaotalk.png";
-import { Map, AdvancedMarker } from "@vis.gl/react-google-maps";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { AdvancedMarker } from "@vis.gl/react-google-maps";
 
 import "./styles/Contact.css";
 
 const Contact = () => {
   const { t } = useTranslation();
+  const [map, setMap] = useState(null);
 
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-  const mapId = "5bfa600c7be908eb";
+  const center = { lat: 36.48883849333951, lng: 127.26052669020419 };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: apiKey,
+  });
+
+  const onLoad = React.useCallback((map) => {
+    const bounds = new window.google.maps.LatLngBounds(
+      new window.google.maps.LatLng(36.488829, 127.2604), // Southwest corner
+      new window.google.maps.LatLng(36.48885, 127.2608) // Northeast corner
+    );
+    map.fitBounds(bounds); // Fit bounds to the map
+    map.setZoom(7); // Set zoom level to show a larger area
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
   return (
     <div>
       <h3 className="contact-title">{t("contact.title")}</h3>
       <div className="contact-container">
         <img className="naver-map" src={map} alt={t("contact.mapAlt")} />
-        <div>
-          <Map
-            apiKey={apiKey}
-            zoom={13}
-            center={{ lat: 36.49020337038061, lng: 127.25945066163874 }}
-            className="map"
-            mapId={mapId}
-          >
-            <AdvancedMarker
-              position={{ lat: 36.49020337038061, lng: 127.25945066163874 }}
-              title="채울미"
-            />
-          </Map>
+        <div className="googleMap-container">
+          {isLoaded ? (
+            <GoogleMap
+              center={center}
+              Zoom={10}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+              zIndex="modal"
+              mapContainerStyle={{ width: "400px", height: "400px" }}
+            >
+              <AdvancedMarker position={center} title="채울미" />
+
+              {/* Child components, such as markers, info windows, etc. */}
+            </GoogleMap>
+          ) : (
+            <div>Loading...</div> // Optional: Display a loading message
+          )}
         </div>
       </div>
       <div className="kakao-container">
