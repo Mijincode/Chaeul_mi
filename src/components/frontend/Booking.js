eaf5afc0295003559b4671b5076b9db767c31fd7
Mijin <mijindoig@hotmail.com>
2024-10-22 11:12:32 +1000

    Delete not being used

--- src/components/frontend/Booking.js
+++ src/components/frontend/Booking.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
