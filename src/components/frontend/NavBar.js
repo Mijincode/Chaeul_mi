af5233975f78cf8a358c72e36c9d6e12444f6fde
Mijin <mijindoig@hotmail.com>
2025-10-02 22:40:56 +1000

    Add hamburger

--- src/components/frontend/NavBar.js
+++ src/components/frontend/NavBar.js
@@ -1,86 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import logo from "../../photos/logoNavbar.png";
 import { useTranslation } from "react-i18next";
+
+import logo from "../../photos/logo10.png";
 import Australia from "../../photos/australia.png";
 import Korea from "../../photos/south-korea.png";
 import "./styles/NavBar.css";
 
-const NavBar = () => {
-  const { i18n } = useTranslation();
-  const { t } = useTranslation();
+export default function NavBar() {
+  const { i18n, t } = useTranslation();
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    const onKey = (e) => e.key === "Escape" && setExpanded(false);
+    document.addEventListener("keydown", onKey);
+    document.body.style.overflow = expanded ? "hidden" : "";
+    return () => {
+      document.removeEventListener("keydown", onKey);
+      document.body.style.overflow = "";
+    };
+  }, [expanded]);
+
+  const closeMenu = () => setExpanded(false);
 
   return (
-    <Navbar className="navbar">
+    <Navbar
+      className="navbar"
+      expand="lg"
+      variant="dark"
+      expanded={expanded}
+      onToggle={setExpanded}
+    >
       <div className="nav-container">
-        <Navbar.Brand as={Link} to="/home">
-          <img src={logo} alt="Logo" className="logo" />
+        <Navbar.Brand
+          as={Link}
+          to="/home"
+          onClick={closeMenu}
+          className="brand"
+        >
+          <img src={logo} alt="채울美" className="brand-logo" />
         </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          className="navbar-toggler"
-        />
-        <Navbar.Collapse id="basic-navbar-nav">
+
+        {/* Hamburger */}
+        <Navbar.Toggle aria-controls="primary-nav" className="navbar-toggler">
+          <span className="navbar-toggler-icon" />
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="primary-nav" className="nav-collapse">
           <Nav className="navbar-link">
-            <Nav.Link as={Link} to="/home">
-              {/* {t("navbar.home")} */}
+            <Nav.Link as={Link} to="/home" onClick={closeMenu}>
               {t("navbar.home")}
             </Nav.Link>
-            <Nav.Link as={Link} to="/gallery">
-              {/* {t("navbar.gallery")} */} {t("navbar.gallery")}
+            <Nav.Link as={Link} to="/gallery" onClick={closeMenu}>
+              {t("navbar.gallery")}
             </Nav.Link>
-            {/* <Nav.Link as={Link} to="/review">
-              {t("navbar.review")}Review
-            </Nav.Link> */}
-            <Nav.Link as={Link} to="/review-phone">
-              {/* {t("navbar.review")} */} {t("navbar.review")}
+            <Nav.Link as={Link} to="/review-phone" onClick={closeMenu}>
+              {t("navbar.review")}
             </Nav.Link>
-            <Nav.Link as={Link} to="/about-us">
-              {/* {t("navbar.about")} */} {t("navbar.aboutUs")}
+            <Nav.Link as={Link} to="/about-us" onClick={closeMenu}>
+              {t("navbar.aboutUs")}
             </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
-              {/* {t("navbar.contact")} */} {t("navbar.contact")}
+            <Nav.Link as={Link} to="/contact" onClick={closeMenu}>
+              {t("navbar.contact")}
             </Nav.Link>
-            <Nav.Link as={Link} to="/faqs">
-              {/* {t("navbar.faqs")} */} {t("navbar.faqs")}
+            <Nav.Link as={Link} to="/faqs" onClick={closeMenu}>
+              {t("navbar.faqs")}
             </Nav.Link>
-
-            {/* <a
-              className="booking-button"
-              href="https://open.kakao.com/o/sJVFkuT"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {t("navbar.button")}
-            </a> */}
 
             <motion.a
               href="https://open.kakao.com/o/sJVFkuT"
               target="_blank"
+              rel="noopener noreferrer"
               className="booking-button"
               whileHover={{ scale: 1.05 }}
-              whileTap={{ scale: 0.95 }}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
+              whileTap={{ scale: 0.96 }}
+              onClick={closeMenu}
             >
               {t("navbar.button")}
             </motion.a>
 
-            <div className="language-switcher">
+            <div
+              className="language-switcher"
+              role="group"
+              aria-label="Language"
+            >
               <img
                 src={Australia}
-                alt="Australian flag"
+                alt="English"
                 className="flag-icon"
-                onClick={() => i18n.changeLanguage("en")}
+                onClick={() => {
+                  i18n.changeLanguage("en");
+                  closeMenu();
+                }}
               />
               <img
                 src={Korea}
-                alt="Korean flag"
+                alt="한국어"
                 className="flag-icon"
-                onClick={() => i18n.changeLanguage("ko")}
+                onClick={() => {
+                  i18n.changeLanguage("ko");
+                  closeMenu();
+                }}
               />
             </div>
           </Nav>
@@ -88,6 +110,4 @@ const NavBar = () => {
       </div>
     </Navbar>
   );
-};
-
-export default NavBar;
+}
